8a2a8d5361adc1554fc4da40f71fab96ccb44dae
반영환 <lopahn2@gmail.com>
2024-04-05 01:45:02 +0900

    feat:

--- src/contract/provider/provider.ts
+++ src/contract/provider/provider.ts
@@ -2,7 +2,7 @@ import { ethers } from "ethers";
 
 // Check if environment variables are defined, otherwise provide defaults or throw an error
 const rpcUrl = process.env.POLYGON_MUMBI_RPC_URL || "default_rpc_url";
-const adminKey = process.env.ADMIN_KEY || "default_admin_key";
+const adminKey = process.env.SUPERVISOR_KEY || "default_admin_key";
 
 export const provider = new ethers.JsonRpcProvider(rpcUrl);
 
